a334c56d61ad1239aa7a0bd10e21ed3d5b6f67c1
Gonzalo Muñoz <gonzalo.munoz.ext@sodexo.com>
2019-10-14 09:54:54 -0300

    Initial commit

---
+++ src/main/java/tech/nullpointerexception/msbeerservice/MsbeerserviceApplication.java
@@ -0,0 +1,13 @@
+package tech.nullpointerexception.msbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsbeerserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsbeerserviceApplication.class, args);
+    }
+
+}
